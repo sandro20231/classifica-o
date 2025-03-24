071caf2741896beaf0087c0e868294c3b8df32c7
Sandro <sandrocoelho2023a@gmail.com>
2025-03-24 11:01:38 -0300

    inicial

---
+++ insert.py
@@ -0,0 +1,21 @@
+import sys
+
+Array = [5, 2]
+i = 0
+
+
+def trocarposicao(Array, i):
+    
+    i = 0
+    Array[i]
+    Array[i+1]
+    tmp = 0
+    tmp = Array[i]
+    Array[i] = Array[i+1]
+    Array[i+1] = tmp
+
+    print("Array[i]:", Array[i])
+    print("array[i+1]", Array[i+1])
+
+
+trocarposicao(Array, i)
